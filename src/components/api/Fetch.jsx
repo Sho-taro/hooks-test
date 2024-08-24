8d3f929d747b9dfb95b73e41ef7e4d6520283423
Shotaro Kikuchi <sho96taro.biz@gmail.com>
2024-08-24 16:44:21 +0900

    [add]
    クエリパラメータを使ったaxios

--- src/components/api/Fetch.jsx
+++ src/components/api/Fetch.jsx
@@ -1,28 +1,44 @@
 import React, { useState, useEffect } from 'react';
-import axios from 'axios'
+import axios from 'axios';
 
 const Fetch = () => {
-    const [posts, setPosts] = useState([])
+	const [posts, setPosts] = useState([]);
+	const [id, setId] = useState('');
 
-    useEffect(() => {
-        // axios.get('https://jsonplaceholder.typicode.com/posts')
-        //     .then(res => {
-        //         setPosts(res.data)
-        //     });
-        fetch('https://jsonplaceholder.typicode.com/posts', { method: 'GET' })
-            .then(res => res.json())
-            .then(data => {
-                setPosts(data)
-            });
-    }, [])
+	const handleInputChange = e => {
+		setId(e.target.value);
+	};
+	const handleClick = () => {
+        axios.get(`https://jsonplaceholder.typicode.com/posts/${id}`)
+            .then(res => {
+			    setPosts([res.data]);
+		    });
+	};
 
-    return (
-        <ul>
-            {posts.map(post => (
-                <li key={post.id}>{ post.title }</li>
-            ))}
-        </ul>
-    );
+	useEffect(() => {
+		axios.get(`https://jsonplaceholder.typicode.com/posts/`).then(res => {
+			setPosts(res.data);
+		});
+		// fetch('https://jsonplaceholder.typicode.com/posts', { method: 'GET' })
+		//     .then(res => res.json())
+		//     .then(data => {
+		//         setPosts(data)
+		//     });
+	}, []);
+
+	return (
+		<div>
+			<p>
+				<input type="text" value={id} onChange={handleInputChange} />
+			</p>
+			<button onClick={handleClick}>Submit</button>
+			<ul>
+				{posts.map(post => (
+					<li key={post.id}>{post.title}</li>
+				))}
+			</ul>
+		</div>
+	);
 };
 
 export default Fetch;
