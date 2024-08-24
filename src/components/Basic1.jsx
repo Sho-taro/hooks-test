ed1748b4fdca083b433484046b3f6327f1ce8782
Shotaro Kikuchi <sho96taro.biz@gmail.com>
2024-08-24 11:58:52 +0900

    [add]
    useStateを追加

--- src/components/Basic1.jsx
+++ src/components/Basic1.jsx
@@ -1,14 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Basic1 = ({ name }) => {
+    const [count, setCount] = useState(0)
     const clickHandler = () => {
-        console.log('clicked')
+        // console.log('clicked')
+        setCount(count+1)
     }
 
     return (
         <>
             <h1>Hello {name}</h1>
-            <button onClick={clickHandler}>Click me</button>
+            <button onClick={clickHandler}>+1</button>
+            <p>{ count }</p>
         </>
     );
 };
