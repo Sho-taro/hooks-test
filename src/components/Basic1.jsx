26f0d3bb592db64b239886da1dbe109ae680e5cd
Shotaro Kikuchi <sho96taro.biz@gmail.com>
2024-08-24 12:07:08 +0900

    [add]
    useState2

--- src/components/Basic1.jsx
+++ src/components/Basic1.jsx
@@ -4,7 +4,9 @@ const Basic1 = ({ name }) => {
     const [count, setCount] = useState(0)
     const clickHandler = () => {
         // console.log('clicked')
-        setCount(count+1)
+        // setCount(count+1)
+        setCount(prev => prev + 1);   // prevはcountの更新前の値
+		setCount(prev => prev + 1);
     }
 
     return (
