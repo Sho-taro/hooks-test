aa5d926f39a416cbda951975fab2d0ec2ee517b4
Shotaro Kikuchi <sho96taro.biz@gmail.com>
2024-08-24 11:50:03 +0900

    [add]
    ボタンおよびイベントハンドラーを追加

--- src/components/Basic1.jsx
+++ src/components/Basic1.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
 
 const Basic1 = ({ name }) => {
+    const clickHandler = () => {
+        console.log('clicked')
+    }
+
     return (
         <>
-            <h1>Hello { name }</h1>
+            <h1>Hello {name}</h1>
+            <button onClick={clickHandler}>Click me</button>
         </>
     );
 };
