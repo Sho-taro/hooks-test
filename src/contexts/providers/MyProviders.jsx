2173fc91f3f0b71148ae584bf2a94f731aae4ef9
Shotaro Kikuchi <sho96taro.biz@gmail.com>
2024-08-26 20:33:25 +0900

    [add]
    複数のProviderをコンポーネントに切り出す

---
+++ src/contexts/providers/MyProviders.jsx
@@ -0,0 +1,23 @@
+import React, { useState } from 'react';
+import ThemeContext from '../ThemeContext';
+import UserContext from '../UserContext';
+
+// 複数のProviderを使用する際は、下記のMyProvidersのようにコンポーネントに切り出した方が可読性が上がる
+const MyProviders = ({ value }) => {
+	const [user, setUser] = useState(null)
+
+	return (
+		<div>
+			<ThemeContext.Provider value={value}>
+				<UserContext.Provider value={{
+					user,
+					setUser,
+				}}>
+					{children}
+				</UserContext.Provider>
+			</ThemeContext.Provider>
+		</div>
+	);
+};
+
+export default MyProviders;
